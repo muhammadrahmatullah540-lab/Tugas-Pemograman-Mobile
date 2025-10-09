97de384095540b99379af2c825c6f9100f99e64d
muhammadrahmatullah540 <muhammadrahmatullah540@gmail.com>
2025-10-10 05:02:18 +0800

    Tugas

---
+++ bin/menggunakan_for_in.dart
@@ -0,0 +1,7 @@
+void main() {
+  var array = <String>['Shana', 'Lia', 'Shia'];
+
+  for (var value in array) {
+    print(value);
+  }
+}
---
+++ bin/tanpa_for_in.dart
@@ -0,0 +1,7 @@
+void main() {
+  var array = <String>['Shana', 'Lia', 'Shia'];
+
+  for (var i = 0; i < array.length; i++) {
+    print(array[i]);
+  }
+}
