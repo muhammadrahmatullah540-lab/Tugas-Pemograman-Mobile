1fc69d1a5626e0175d3c2c5cae8eb1c53a2d58a7
muhammadrahmatullah540 <muhammadrahmatullah540@gmail.com>
2025-10-10 04:33:09 +0800

    Tugas

--- bin/mengakses_nullable_member.dart
+++ bin/mengakses_nullable_member.dart
@@ -1,6 +1,6 @@
 void main() {
-  int intNumber = 10;  // Drop the ? since it's non-null
-  double doubleNumber = intNumber.toDouble();  // No need for ?. or ??
+  int intNumber = 10; 
+  double doubleNumber = intNumber.toDouble();  
 
-  print(doubleNumber);  // Outputs: 10.0
+  print(doubleNumber);  
 }
