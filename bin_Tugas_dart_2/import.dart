fce08d8200374f792f1f5503f3df0ddf2928568f
muhammadrahmatullah540 <muhammadrahmatullah540@gmail.com>
2025-11-15 14:49:49 +0800

    project1

--- bin_Tugas_dart_2/import.dart
+++ bin_Tugas_dart_2/import.dart
@@ -1,15 +1,9 @@
-// File: project_1/bin_Tugas_dartt_2/class.dart
+import 'category.dart';
 
-class Category {
-  String id = "";
-  String name = "";
 
-  // Constructor
-  Category(this.id, this.name);
-}
+void main(){
+var category = Category("1", "Gadget");
 
-void main() {
-  var category = Category("1", "Gadget");
-  print(category.id);
-  print(category.name);
+print (category.id);
+print(category.name);
 }
